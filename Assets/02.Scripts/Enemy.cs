61d45b5aab0ddeafd1c06abc9eb6d89e016dc694
Jeje <wpwpgjs@gmail.com>
2025-11-06 18:48:23 +0900

    Object Pooling Commit

--- Assets/02.Scripts/Enemy.cs
+++ Assets/02.Scripts/Enemy.cs
@@ -2,39 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 에너미를 제어하는 클래스
 public class Enemy : MonoBehaviour
 {
+    // 에너미의 이동 속도
     public float speed;
+
+    // 추적할 대상
     public Rigidbody2D target;
 
-    bool isLive = true;
+    // 에너미가 살아있는 상태인지
+    bool isLive = false;
 
+    // 에너미 Rigidbody2D 컴포넌트
     Rigidbody2D enemyRigid;
+
+    // 에너미 SpriteRenderer 컴포넌트
     SpriteRenderer enemySprite;
-    // Start is called before the first frame update
+
+    // 게임 시작 전에 컴포넌트를 가져오는 초기 설정
     void Awake()
     {
+        // Rigidbody2D 컴포넌트 가져오기
         enemyRigid = GetComponent<Rigidbody2D>();
+
+        // SpriteRenderer 컴포넌트 가져오기
         enemySprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    // 물리 연산이 필요한 경우 매 프레임마다 호출됨
     void FixedUpdate()
     {
+        // 적이 살아있으면 움직이지 않음
         if (isLive)
             return;
 
+        // 대상과 에너미 사이의 방향 벡터 계산
         Vector2 dirVec = target.position - enemyRigid.position;
+
+        // 방향 벡터를 정규화하고 속도와 시간에 따라 이동 벡터 계산
         Vector2 moveVec = dirVec.normalized * speed * Time.deltaTime;
+
+        // 에너미를 새로운 위치로 이동
         enemyRigid.MovePosition(enemyRigid.position + moveVec);
+
+        // 이동 후 속도 초기화 (불필요한 물리 효과 방지)
         enemyRigid.velocity = Vector2.zero;
     }
 
+    // 렌더링 직전에 호출되어 이미지 방향을 조정
     private void LateUpdate()
     {
+        // 적이 살아있으면 이미지 방향 변경하지 않음
         if (isLive)
             return;
 
+        // 대상이 왼쪽에 있으면 이미지 좌우 반전
         enemySprite.flipX = target.position.x < enemyRigid.position.x;
+    }
+
+    void OnEnable()
+    {
+        target = GameManager.instance.playerController.GetComponent<Rigidbody2D>();
     }
 }
--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using Goldmetal.UndeadSurvivor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public PoolManager poolManager;
     public PlayerController playerController;
 
     private void Awake()
